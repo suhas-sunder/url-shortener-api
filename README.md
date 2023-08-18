0124c1c7b40e7666565fe0668a5b06ae1d14b459
suhas-sunder <77464593+suhas-sunder@users.noreply.github.com>
2023-08-18 17:48:18 -0400

    feat: working url shortener
    -Add URL shortening functionality. -Add table that outputs list of shortened URLs. -Add functionality to remove old URLs from table.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # url-shortener-api
-This project uses an API to shorten a URL. The API used for this project is https://shrtco.de/docs. This is a practice project for learning purposes only. 
+
+This project uses an API to shorten a URL. The API used for this project is https://shrtco.de/docs. This is a practice project for learning purposes only.
---
+++ src/App.js
@@ -0,0 +1,95 @@
+import "./App.css";
+import { useState, useRef, useEffect } from "react";
+
+function App() {
+  const [links, setLinks] = useState([]);
+  const [isFocused, setIsFocused] = useState("default");
+  const [err, setErr] = useState("");
+  const inputRef = useRef("");
+
+  const handleShortener = () => {
+    const inputValue = inputRef.current.value;
+    if (inputValue) {
+      try {
+        fetch(`https://api.shrtco.de/v2/shorten?url=${inputValue}`)
+          .then((res) => res.json())
+          .then((data) => {
+            if (data.ok) {
+              console.log(data.result.code);
+              inputRef.current.value = ""; //Reset input text
+              setLinks((prevState) => [
+                ...prevState,
+                {
+                  id: data.result.code,
+                  short: data.result.short_link,
+                  original: data.result.original_link,
+                },
+              ]);
+            } else {
+              setErr(`${data.error.split(",")[0]}, please try again!`);
+            }
+          });
+      } catch {
+        console.log("Failed to fetch data.");
+      }
+    }
+  };
+
+  // Delete URL from list
+  const handleDeleteURL = (id) => {
+    setLinks(links.filter((link) => link.id !== id));
+  };
+
+  // Handle error message for empty input
+  useEffect(() => {
+    console.log(links);
+    const inputValue = inputRef.current.value;
+    if (isFocused === "false") {
+      if (inputValue === "") {
+        setErr("Please enter a valid URL");
+      }
+    } else {
+      setErr(""); //Reset error onBlur
+    }
+  }, [isFocused]);
+
+  return (
+    <div className="wrapper">
+      <h1>URL Shortener</h1>
+      <div>
+        <input
+          ref={inputRef}
+          placeholder="Enter URL"
+          onFocus={() => setIsFocused("true")}
+          onBlur={() => setIsFocused("false")}
+        ></input>
+        <button onClick={handleShortener}>Shorten</button>
+      </div>
+      <p>{err}</p>
+      {links.length > 0 && (
+        <table>
+          <thead>
+            <tr>
+              <td>Original</td>
+              <td>Short</td>
+            </tr>
+          </thead>
+          <tbody>
+            {links.map((link) => (
+              <tr key={link.id}>
+                <td>{link.original}</td>
+                <td>{link.short}</td>
+                <td>
+                  <button>Copy</button>
+                  <button onClick={() => handleDeleteURL(link.id)}>X</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+}
+
+export default App;
